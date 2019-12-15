fb5ffebc14b9573f2cb6bc350972d86f4a5a5708
Tseng Kuan Wei <wayne@Tsengde-MacBook-Pro.local>
2019-12-15 14:30:52 +0800

    os.path.join

--- src/02.cutregion.py
+++ src/02.cutregion.py
@@ -6,48 +6,18 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('imgdir',help='image file path')
 parser.add_argument('-o' ,help = 'output image directory + name',default = 'cut/img')
-parser.add_argument('-to', help = 'output image format (.jpg or .bmp)', default = '.jpg')
 
 args = parser.parse_args()
 imgpath = args.imgdir
 
 # File I/O:
 images = glob.glob(imgpath+'*')
-outpath = args.o.split('/')[0]
+outpath = os.path.join(args.imgdir,'../',args.o.split('/')[0])
 
-if os.path.exists(outpath):
-    os.system('rm -f {}/*'.format(outpath))
-else:
-    os.mkdir(outpath)
-
-
-nof = len(images)
+os.system('rm -rf {}'.format(outpath))
+os.mkdir(outpath)
 
 for fname in images:
-    
     img = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
-
     cutimg = naiiveCut(img)
-
-    cv2.imwrite('{}{:03d}{}'.format(args.o,cnt,args.to), cut_img)
-
-
-
-
-
-
-for freq in freqs:
-    print(freq)
-    os.mkdir('cutt/{}'.format(freq))
-    frame_dir = 'frames/{}'.format(freq)
-    
-    center = [[], []]
-    radius = []
-    
-    pics = os.listdir(frame_dir)
-    for i in range(len(pics)):
-        pic = '{}.bmp'.format(i)
-        img = cv2.imread(os.path.join(frame_dir, pic), cv2.IMREAD_GRAYSCALE)
-        print('    ', pic)
-        cutimg = naiiveCut(img)
-        cv2.imwrite('cutt/{}/{}.bmp'.format(freq, i), cutimg)
+    cv2.imwrite(os.path.join(outpath,fname.split('/')[-1]), cutimg)
