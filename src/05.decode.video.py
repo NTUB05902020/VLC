0af66382b865a1d4503125140da57e1f0120a812
Tseng Kuan Wei <wayne@Tsengde-MacBook-Pro.local>
2019-12-15 14:45:59 +0800

    refine the code

--- src/05.decode.video.py
+++ src/05.decode.video.py
@@ -3,8 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from util import naiiveCut, extract1DSignal, judgeBit
 
-symbol2str = lambda symbol: '1' if symbol==1 else ('0' if symbol==-1 else '?')
+total_bits = 12
+nof_prem = 6
+nof_data = 4
+nof_splt = 2
 
+total_frames = 6 + 4*total_bits + 2*total_bits
+rand_3200_ratio = 0.8
+rand_weights = [1-rand_3200_ratio, rand_3200_ratio]
+
+symbol2str = lambda symbol: '1' if symbol==1 else ('0' if symbol==-1 else '?')
 
 def printSymbols(symbol_string):
     for symbol in symbol_string: print(symbol2str(symbol), end=' ')
@@ -21,35 +29,33 @@ def printSignal(symbol_string, bit_string):
         for j in range(i,i+4): print(symbol2str(bit_string[j]), end=' ')
         print('  -->   {}'.format(symbol2str(symbol_string[i//6-1])))
 
+if __name__ == '__main__':
+    try:
+        frame_dir = str(sys.argv[1])
+    except IndexError:
+        print('Format: {} [frame_dir]'.format(sys.argv[0]))
+        sys.exit(1)
 
-try:
-    video_dir = str(sys.argv[1])
-except IndexError:
-    print('Format: {} [video_dir]'.format(sys.argv[0]))
-    sys.exit(1)
+    pics = os.listdir(frame_dir)
+    imgtype = pics[0].split('.')[-1]
 
-pics = sorted(os.listdir(video_dir)) # glob
-total_bits = 12
-total_frames = 6 + 4*total_bits + 2*total_bits
-bit_string, symbol_string = [], []
-
-rand_3200_ratio = 0.8
-rand_weights = [1-rand_3200_ratio, rand_3200_ratio]
-
-
-for i in range(total_frames):
-    gray_frame = cv2.imread('{}/{}.bmp'.format(video_dir, i), cv2.IMREAD_GRAYSCALE)
-    cutted = naiiveCut(gray_frame)
-    sig = extract1DSignal(cutted)
+    total_frames = nof_prem + (nof_data+nof_splt) * total_bits
     
-    bit, res = judgeBit(sig)
-    bit_string.append(bit)
-    
-    if i>=6 and (i-6)%6 == 4:
-        nums = [0, 0, 0]  #[monenum, zeronum, onenum]
-        for j in range(i-4, i): nums[bit_string[j]+1] += 1
-        if nums[0] > nums[2]: symbol_string.append(-1)
-        elif nums[0] < nums[2]: symbol_string.append(1)
-        else: symbol_string.append(random.choices([-1,1], weights=rand_weights)[0])
+    bit_string, symbol_string = [], []
 
-printSignal(symbol_string, bit_string)
+    for i in range(total_frames):
+        gray_frame = cv2.imread('{}/{}.{}'.format(frame_dir,i,imgtype), cv2.IMREAD_GRAYSCALE)
+        cutted = naiiveCut(gray_frame)
+        sig = extract1DSignal(cutted)
+        
+        bit, res = judgeBit(sig)
+        bit_string.append(bit)
+        
+        if i>=nof_prem and (i-nof_prem)%(nof_data+nof_splt) == nof_data:
+            nums = [0, 0, 0]  #[monenum, zeronum, onenum]
+            for j in range(i-nof_data, i): nums[bit_string[j]+1] += 1
+            if nums[0] > nums[2]: symbol_string.append(-1)
+            elif nums[0] < nums[2]: symbol_string.append(1)
+            else: symbol_string.append(random.choices([-1,1], weights=rand_weights)[0])
+
+    printSignal(symbol_string, bit_string)
