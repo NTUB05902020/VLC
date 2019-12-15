e22bdec4c296732188701d5b0a2ca088ac88b67c
NTUB05902020 <b05902020@ntu.edu.tw>
2019-12-15 16:10:00 +0800

    fixed '/' problems

--- src/02.cutregion.py
+++ src/02.cutregion.py
@@ -1,23 +1,29 @@
 import cv2, os, glob, argparse
 import numpy as np
-from util import naiiveCut
+from util import cutRegion, extract1DSignal
+from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('imgdir',help='image file path')
-parser.add_argument('-o' ,help = 'output image directory + name',default = 'cut/img')
+parser.add_argument('-o' ,help = 'output image directory + name',default = 'cut/')
 
 args = parser.parse_args()
 imgpath = args.imgdir
 
 # File I/O:
 images = glob.glob(imgpath+'*')
-outpath = os.path.join(args.imgdir,'../',args.o.split('/')[0])
+outpath = args.imgdir.strip('/').split('/')
+outpath = os.path.join(*outpath[:-1], args.o)
 
 os.system('rm -rf {}'.format(outpath))
 os.mkdir(outpath)
 
-for fname in images:
+for i,fname in enumerate(images):
     img = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
-    cutimg = naiiveCut(img)
+    cutimg = cutRegion(img)
     cv2.imwrite(os.path.join(outpath,fname.split('/')[-1]), cutimg)
+    if i == 0:
+        sig = extract1DSignal(cutimg)
+        plt.plot(sig, '.')
+        plt.savefig('hello.png',)
