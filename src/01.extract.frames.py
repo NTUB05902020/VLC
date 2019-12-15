43ed313e48ef67d77e8fe56dd46221b1911c7ca3
Tseng Kuan Wei <wayne@Tsengde-MacBook-Pro.local>
2019-12-15 13:53:03 +0800

    03d

--- src/01.extract.frames.py
+++ src/01.extract.frames.py
@@ -24,7 +24,7 @@ while(True):
     ret, img = vid.read()
     if ret:
         gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('{}{}{}'.format(args.o,str(cnt),args.to), gray_frame)
+        cv2.imwrite('{}{:03d}{}'.format(args.o,str(cnt),args.to), gray_frame)
         cnt += 1
     else:
         break
